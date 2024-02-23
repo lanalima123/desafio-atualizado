ac65266a73bc0e97b67afb42f4d75fd177777aab
lanalima123 <alcylima03@gmail.com>
2024-02-23 15:24:19 -0300

    meu primeiro desafio alura

---
+++ linguagemJS/linguagem.js
@@ -0,0 +1,19 @@
+let textInput = document.querySelector("#input__texto")
+let textOutput = document.querySelector("#output__texto")
+
+function cripto()
+{
+    var texto = textInput.value;
+    var resultCripto = texto.replace(/e/g, 'enter').replace(/i/g, "imes").replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat')
+    document.querySelector('h2').innerHTML= resultCripto
+}
+function descripto(){
+    var textoCripto= textInput.value
+    var resultDescripto = textoCripto.replace(/enter/g, 'e').replace(/imes/g, "i").replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u')
+    document.querySelector('h2').innerHTML= resultDescripto
+
+} 
+
+function copiarTexto(){
+    document.querySelector(textarea).innerHTML = ' '
+}
